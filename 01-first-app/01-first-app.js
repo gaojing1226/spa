e788d1adb6be9c1d372adc90338bf6726f9e6c2b
gaojing <646040659@qq.com>
2018-03-08 16:43:35 +0800

    修改 0.1+0.2 不等于0.3的bug; 增加焦点自动聚集到第一个文本框

--- 01-first-app/01-first-app.js
+++ 01-first-app/01-first-app.js
@@ -36,13 +36,21 @@ $(function(){
 		//计算周长 面积
 		$(".rectangle__calc").click(function(){
     		var width = Number($(".rectangle__width").val());
-    		//console.log(width);
     		var height = Number($(".rectangle__height").val());
-    		//console.log(height);
-    		var perimeter = (width+height)*2;
-    		//console.log(perimeter);
-    		var area = width*height;
-    		//console.log(area);
+
+    		if(width<1||height<1){
+    			var n1 = width.toString().split(".")[1].length;
+    			var n2 = height.toString().split(".")[1].length;
+    			var times = Math.pow(10, Math.max(n1, n2));
+    		}
+    		else{
+    			times=1;
+    		}
+
+    		var perimeter = (width*times+height*times)/times*2;
+
+    		var area = width*times*height*times/times/times;
+
 
     		$('.rectangle__perimeter').val(perimeter);
     		$('.rectangle__area').val(area);
