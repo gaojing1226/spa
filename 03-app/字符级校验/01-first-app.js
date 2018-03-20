1d23949947c63bb78fc256d728f68c0ad2a87067
gaojing <646040659@qq.com>
2018-03-20 08:54:37 +0800

    修改字符级、字段级校验

---
+++ 03-app/字符级校验/01-first-app.js
@@ -0,0 +1,72 @@
+$(function(){
+    $('form :input').blur(function(){
+        //去掉先前的提醒
+        var $parent = $(this).parent();
+        $parent.find(".formtips").remove();
+        //判断宽度为空或非数字
+        if($(this).is('.rectangle__width')){
+            if(this.value=="" ){
+                var errorMsg1 = '宽度不能为空!';
+                $(this).next().after('<div class="formtips onError1">'+errorMsg1+'</div>');
+            }
+            else if(this.value < 0 ){
+                var errorMsg2 = '宽度必须大于0!';
+                $(this).next().after('<div class="formtips onError2">'+errorMsg2+'</div>');
+            }
+            //exec()找到了匹配的文本,则返回一个结果数组。否则,返回 null
+            else{
+                $(".rectangle__height").removeAttr("disabled");
+            }
+        }
+
+        //判断长度为空或非数字
+        if($(this).is('.rectangle__height')){
+            if(this.value=="" ){
+                var errorMsg4 = '高度不能为空!';
+                $(this).next().after('<div class="formtips onError4">'+errorMsg4+'</div>');
+            }
+            else if(this.value<0 ){
+                var errorMsg5 = '高度必须大于0!';
+                $(this).next().after('<div class="formtips onError5">'+errorMsg5+'</div>');
+            }
+        }
+
+    })
+
+    $('.rectangle__width').keypress(function(e){
+        validateKey(e);
+    });
+        
+    $('.rectangle__height').keypress(function(e){
+        validateKey(e);
+    });
+
+    function validateKey(e) {
+        if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+\[\]{}|;:'",<>/?\\]/.test(e.key)){
+            e.preventDefault();
+        }
+            
+    }
+        //计算周长 面积
+    $(".rectangle__calc").click(function(){
+        var width = Number($(".rectangle__width").val());
+        var height = Number($(".rectangle__height").val());
+
+        if(width<1||height<1){
+            var n1 = width.toString().split(".")[1].length;
+            var n2 = height.toString().split(".")[1].length;
+            var times = Math.pow(10, Math.max(n1, n2));
+        }
+        else{
+            times=1;
+        }
+
+        var perimeter = (width*times+height*times)/times*2;
+
+        var area = width*times*height*times/times/times;
+
+
+        $('.rectangle__perimeter').val(perimeter);
+        $('.rectangle__area').val(area);
+    });
+});
