70ed33323fafd9118c04e1d3407843952d10b7d0
gaojing <646040659@qq.com>
2018-03-27 14:19:36 +0800

    组件化密码可见

--- 02-app/02-password/02-password.js
+++ 02-app/02-password/02-password.js
@@ -1,10 +1,33 @@
-window.onload=function(){
-	var btn=document.getElementById("btn");
-	var pass=document.getElementById("pass")
-	btn.onmouseover=function(){
-		pass.type="text"
-	};
-	btn.onmouseup=btn.onmouseout=function(){
-		pass.type="password"
+
+//容器 config
+	var PasswordBox=function(config){ 
+		var $pwd=$('<input type="password" id="pass" />');
+		var $pwdText=$('<input type="text"></input>');
+		var $eyeImage=$('<div></div>');
+		var $div = $('<div class="main">密码：</div>');
+
+		$div.append($pwd);
+		$div.append($eyeImage);
+		$div.append($pwdText);
+
+		$(config.container).append($div);
+
+		$pwd.on('input',function(){
+			console.log("1");
+			$pwdText.val($pwd.val());
+		})
+
+		$eyeImage.mouseout(function(){
+			$pwdText.css('z-index',10);
+
+		})
+
+		$eyeImage.mouseover(function(){
+			$pwdText.css('z-index',-10);
+			
+		})
+
+		this.getPwd=function(){
+			return $pwdText.val();
+		}
 	}
-}
